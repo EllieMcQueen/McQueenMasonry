0276c11218fb72e1e23d7861f2e943f1e0f1c892
EllieMcQueen <70542907+EllieMcQueen@users.noreply.github.com>
2021-02-16 08:42:07 -0700

    link changes

--- mcqueenmasonry/src/Components/Header.js
+++ mcqueenmasonry/src/Components/Header.js
@@ -5,25 +5,25 @@ const Header = ()=>{
     window.onscroll = function () {
         let currentScrollPos = window.pageYOffset;
         let homeLink = document.getElementsByClassName("home")[0];
-        // let servicesLink = document.getElementsByClassName("services")[0];
+        let servicesLink = document.getElementsByClassName("services")[0];
         let projectsLink = document.getElementsByClassName("projects")[0];
         let contactLink = document.getElementsByClassName("contact")[0];
             if (currentScrollPos > 640 && currentScrollPos < 1350) {
             document.getElementById("navbar").style.top = "0";
-      homeLink.classList.add("yellow");
-      // servicesLink.classList.add("white");
+      // homeLink.classList.add("yellow");
+      servicesLink.classList.add("white");
       projectsLink.classList.remove("white");
       contactLink.classList.remove("white");
     } 
     else if (currentScrollPos > 1350 && currentScrollPos < 3300) {
       document.getElementById("navbar").style.top = "0";
-      // servicesLink.classList.add("white");
+      servicesLink.classList.add("white");
       projectsLink.classList.add("white");
       contactLink.classList.remove("white");
     } 
     else if (currentScrollPos > 3300) {
       document.getElementById("navbar").style.top = "0";
-      // servicesLink.classList.add("white");
+      servicesLink.classList.add("white");
       projectsLink.classList.remove("white");
       contactLink.classList.add("white");
     } 
@@ -40,21 +40,21 @@ const Header = ()=>{
         {/* <a href="https://mcqueenmasonry.com/"></a>
         goes between title> and </h1> */}
         <nav className="main-nav">
-           {/* <div className="index">HOME </div> 
-          <div className="index"> SERVICES</div> */}
-          <div className="index"><a href="#" id="nav-home">HOME</a></div>
-          {/* <div className="index"><a href="#services">SERVICES</a></div> */}
-          <div className="index"><a href="#projects">PROJECTS</a></div>
+           <div className="index"> HOME </div> 
+          {/* <div className="index"> SERVICES </div>  */}
+         {/* <div className="index"><a href="#" id="nav-home">HOME</a></div> */}
+          <div className="index"><a href="#services">SERVICES</a></div>
+          <div className="index"><a href="#projects">PROJECTS</a></div> 
           <div className="index"><a href="#contact">CONTACT</a></div>
         </nav>
         <nav id="navbar" className="floating-nav">
-          {/* <div className="index">HOME </div> 
-          <div className="index">  SERVICES</div> */}
-          <div className="index"><a className="home" href="#">HOME</a></div>
-          {/* <div className="index"><a className="services" href="#services">SERVICES</a></div> */}
-          <div className="index"><a className="projects" href="#projects">PROJECTS</a></div>
+         <div className="index">HOME </div> 
+          {/* <div className="index">  SERVICES</div>  */}
+          {/*<div className="index"><a className="home" href="#">HOME</a></div> */}
+          <div className="index"><a className="services" href="#services">SERVICES</a></div>
+         <div className="index"><a className="projects" href="#projects">PROJECTS</a></div>
           <div className="index"><a className="contact" href="#contact">CONTACT</a></div>
-        </nav>
+        </nav> 
       </div>
     </header>
   );
